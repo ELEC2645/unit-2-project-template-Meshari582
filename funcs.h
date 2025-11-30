f4655f2c46b2346196fa2eb3fd48921aa8ef911f
Meshari582 <fy222ma@leeds.ac.uk>
2025-11-30 21:02:02 +0000

    Update funcs.h

--- funcs.h
+++ funcs.h
@@ -1,9 +1,11 @@
 #ifndef FUNCS_H
 #define FUNCS_H
 
-void menu_item_1(void);
-void menu_item_2(void);
-void menu_item_3(void);
-void menu_item_4(void);
+void voltage_divider(void);
+void series_resistors(void);
+void parallel_resistors_2(void);
+void power_vi(void);
+void capacitive_reactance(void);
+void inductive_reactance(void);
 
 #endif
